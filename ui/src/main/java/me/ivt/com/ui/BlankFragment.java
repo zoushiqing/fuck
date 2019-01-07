86abe2eba62535ccd83d9cec5ce009e3f65e4254
love66 <542920864@qq.com>
2019-01-07 23:13:50 +0800

    1.7home

--- ui/src/main/java/me/ivt/com/ui/BlankFragment.java
+++ ui/src/main/java/me/ivt/com/ui/BlankFragment.java
@@ -103,6 +103,7 @@ public class BlankFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
+        
         void onFragmentInteraction(Uri uri);
     }
 }
